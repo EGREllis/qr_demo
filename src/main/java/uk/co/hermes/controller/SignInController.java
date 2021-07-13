3276b731d6ce596b2770f0c8e82b5e31313139bd
Edward Ellis <edwardellis.egre@gmail.com>
2021-07-13 06:25:55 +0100

    Sign in uses cookies to track email and password

--- src/main/java/uk/co/hermes/controller/SignInController.java
+++ src/main/java/uk/co/hermes/controller/SignInController.java
@@ -2,22 +2,20 @@ package uk.co.hermes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import uk.co.hermes.dao.DatabaseDao;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Map;
 
 @Controller
 public class SignInController {
     @Autowired
     private DatabaseDao databaseDao;
-
-    @GetMapping("/")
-    public String index() {
-        return "sign_in";
-    }
 
     @GetMapping("/sign_in")
     public String signIn() {
@@ -27,13 +25,33 @@ public class SignInController {
     @PostMapping("/sign_in")
     public String processSignIn(@RequestParam("email")String email,
                                 @RequestParam("password")String password,
-                                Map<String, Object> model) {
+                                Map<String, Object> model, HttpServletResponse response) {
         Integer userId = null;
         if ( (userId = databaseDao.signIn(email, password)) > 0) {
+            trackUserWithCookies(email, password, response);
             return "welcome";
         } else {
             return "sign_in";
         }
     }
 
+    @GetMapping("/")
+    public String indexWithCookie(@CookieValue(value = "email", defaultValue = "") String email,
+                                  @CookieValue(value = "password", defaultValue = "") String password,
+                                  Map<String, Object> model, HttpServletResponse response) {
+        Integer userId = null;
+        if ( (userId = databaseDao.signIn(email, password)) > 0) {
+            trackUserWithCookies(email, password, response);
+            return "welcome";
+        } else {
+            return "sign_in";
+        }
+    }
+
+    private void trackUserWithCookies(String email, String password, HttpServletResponse response) {
+        Cookie userCookie = new Cookie("email", email);
+        Cookie passwordCookie = new Cookie("password", password);
+        response.addCookie(userCookie);
+        response.addCookie(passwordCookie);
+    }
 }
