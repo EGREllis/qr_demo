a49bfb48a1d3d2f0d55001e765e77cac9b07a083
Edward Ellis <edwardellis.egre@gmail.com>
2021-07-13 16:07:25 +0100

    Removed debugging controllers, templates, added sign out

--- src/main/java/uk/co/hermes/controller/SignInController.java
+++ src/main/java/uk/co/hermes/controller/SignInController.java
@@ -26,6 +26,7 @@ public class SignInController {
     public String processSignIn(@RequestParam("email")String email,
                                 @RequestParam("password")String password,
                                 Map<String, Object> model, HttpServletResponse response) {
+        System.err.println("Process sign in email:"+email+" password:"+password);
         Integer userId = null;
         if ( (userId = databaseDao.signIn(email, password)) > 0) {
             trackUserWithCookies(email, password, response);
@@ -39,6 +40,7 @@ public class SignInController {
     public String indexWithCookie(@CookieValue(value = "email", defaultValue = "") String email,
                                   @CookieValue(value = "password", defaultValue = "") String password,
                                   Map<String, Object> model, HttpServletResponse response) {
+        System.err.println("Index with cookie:"+email+" password:"+password);
         Integer userId = null;
         if ( (userId = databaseDao.signIn(email, password)) > 0) {
             trackUserWithCookies(email, password, response);
@@ -46,6 +48,51 @@ public class SignInController {
         } else {
             return "sign_in";
         }
+    }
+
+    @GetMapping("/register")
+    public String getRegister() {
+        return "register/register";
+    }
+
+    @GetMapping("/sign_out")
+    public String getSignOut(HttpServletResponse response) {
+        trackUserWithCookies("","", response);
+        return "sign_in";
+    }
+
+    @PostMapping("/register")
+    public String postRegister(@RequestParam("email") String email,
+                               @RequestParam("password") String password,
+                               @RequestParam("confirm") String confirm,
+                               Map<String,Object> model,
+                               HttpServletResponse response) {
+        System.err.println("Post register email:"+email+" password:"+password);
+        boolean isAccountAvailable = databaseDao.isAccountAvailable(email);
+        boolean isConfirmEqualPassword = confirm.equals(password);
+        boolean isConfirmOrPasswordLengthGreaterThanFour = confirm.length() > 4 && password.length() > 4;
+        if (    isAccountAvailable &&
+                isConfirmEqualPassword &&
+                isConfirmOrPasswordLengthGreaterThanFour) {
+            // Happy case, email not registered, confirm matches password, proceed with registration
+            int userId = databaseDao.registerAccount(email, password);
+            int signIn = databaseDao.signIn(email, password);
+            if (userId > 0 && signIn > 0) {
+                trackUserWithCookies(email, password, response);
+                return "welcome";
+            }
+        }
+        // Detect and report validation errors
+        if (!isAccountAvailable) {
+            model.put("emailError", "An account for this email address already exists.");
+        }
+        if (!isConfirmEqualPassword) {
+            model.put("confirmError", "Confirmation field does not match the Password field, please try again.");
+        }
+        if (!isConfirmOrPasswordLengthGreaterThanFour) {
+            model.put("passwordError", "Password field needs to be at least 4 characters long.");
+        }
+        return "register/register";
     }
 
     private void trackUserWithCookies(String email, String password, HttpServletResponse response) {
